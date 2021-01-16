274f601e01444634a2fb375f319e47132640c7e5
yeeland <yeelandzhang@icloud.com>
2021-01-16 15:45:28 +0800

    project3

--- project3.py
+++ project3.py
@@ -6,36 +6,35 @@ from torch.utils.data import DataLoader
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, in_dim):
         super(Net, self).__init__()
-        # 结构中只有输入和输出层
-        self.fc1 = nn.Linear(784, 10)
-        # 给一个激活函数,dim=1是第一个维度，即输出第一个维度的概率
-        self.softmax = nn.Softmax(dim=1)
+        self.layer_1 = nn.Sequential(nn.Linear(in_dim, 300), nn.BatchNorm1d(300), nn.ReLU(True))
+        self.layer_2 = nn.Sequential(nn.Linear(300, 100), nn.BatchNorm1d(100), nn.ReLU(True))
+        self.output = nn.Linear(100, 10)
 
     def forward(self, x):
-        # 全连接层把(64, 1, 28, 28)转换为二维(64, 784),view相当于reshape,784=1*28*28
-        x = x.view(x.size()[0], -1)
-        x = self.fc1(x)
-        x = self.softmax(x)
+        x = self.layer_1(x)
+        x = self.layer_2(x)
+        x = self.output(x)
         return x
 
 
 if __name__ == '__main__':
+    # （1）导入数据集
     # 训练集
     train_data = datasets.MNIST(root='./',
                                 train=True,
                                 transform=transforms.ToTensor(),
-                                download=True)
+                                download=False)
     # 测试集
     test_data = datasets.MNIST(root='./',
                                train=False,
                                transform=transforms.ToTensor(),
-                               download=True)
-
-    # 批次大小，即一次加载多少数据
-    batch_size = 64
-    # 装载训练集，shuffle=True将数据打乱
+                               download=False)
+    # （2）数据装载
+    # 批次大小
+    batch_size = 100
+    # 装载训练集，将数据打乱
     train_load = DataLoader(dataset=train_data,
                             batch_size=batch_size,
                             shuffle=True)
@@ -44,53 +43,64 @@ if __name__ == '__main__':
                            batch_size=batch_size,
                            shuffle=True)
 
-    # for i, data in enumerate(train_load):
-    #     inputs, labels = data
-    #     print(inputs.shape)
-    #     print(labels.shape)
-    #     print(labels)
-    #     break
+    # (3)构建模型
+    # 网络的结构输入为28*28，第一层隐藏层的输出为300，第二层隐藏层的输出为100，最后输出为10
+    model = Net(28 * 28)
 
-    # 定义模型,损失函数，优化器
-    model = Net()
-    mse_loss = nn.MSELoss()
+    # (4)定义损失函数和优化器
+    criterion = nn.CrossEntropyLoss()
+    # mse_loss = nn.MSELoss()
     opt = optim.SGD(model.parameters(), lr=0.5)
 
 
+    # (5)训练
     def train():
-        for i, data in enumerate(train_load):
+        train_loss = 0
+        for data in train_load:
             # 每一次迭代都返回一组输入数据和标签
-            input_data, labels = data
-            # 获得模型的结果
-            out = model(input_data)
-            # (64)——>(64, 1)
-            labels = labels.reshape(-1, 1)
-            # 转换为独热编码
-            one_hot = torch.zeros(input_data.shape[0], 10).scatter(1, labels, 1)
-            # 计算loss,out, one_hot的shape要一致
-            loss = mse_loss(out, one_hot)
+            img, label = data
+            # 将图片进行image转换
+            img = img.view(img.size(0), -1)
+            # 构造变量
+            img = Variable(img)
+            label = Variable(label)
+            # 获得模型的计算结果
+            out = model(img)
+            # 计算损失值
+            loss = criterion(out, label)
+            train_loss += loss.data
             # 梯度清零
             opt.zero_grad()
-            # 计算梯度
+            # 损失值传播
             loss.backward()
-            # 修改权值
+            # 优化器优化
             opt.step()
+        print('train_loss={:.6f}'.format((train_loss / len(train_load))), end=',')
 
 
-    # 定义一个测试数据的函数
+    # (6)测试
     def test():
         correct = 0
-        for i, data in enumerate(test_load):
+        test_loss = 0
+        for data in test_load:
             # 每一次迭代都返回一组输入数据和标签
-            input_data, labels = data
+            img, label = data
+            # 将图片进行image转换
+            img = img.view(img.size(0), -1)
+            # 构造变量
+            img = Variable(img)
+            label = Variable(label)
             # 获得模型的结果
-            out = model(input_data)
-            # 获得第一个维度的最大值，以及最大值所在的位置
-            max_value, pred_index = torch.max(out, 1)
-            # 用这64个预测数据与标签做一个对比，统计预测正确的数量
-            correct += (pred_index == labels).sum()
-
-        print('准确率：{0}'.format(correct.item() / len(test_data)))
+            out = model(img)
+            # 计算损失值
+            loss = criterion(out, label)
+            test_loss += loss
+            # 计算准确率
+            _, predict = torch.max(out, 1)
+            equal = predict == label
+            correct += torch.sum(equal)
+        print('test_loss:{:.6f}'.format(test_loss / len(test_load)), end=',')
+        print('accuracy:{:.6f}'.format(correct / len(test_load)))
 
 
     # 训练和测试10个周期
@@ -98,4 +108,3 @@ if __name__ == '__main__':
         print(i, ':', end='')
         train()
         test()
-
