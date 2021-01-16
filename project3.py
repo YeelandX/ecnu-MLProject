d30559331e604dffd9f5180ea4920d3796647fbe
yeeland <yeelandzhang@icloud.com>
2021-01-16 12:54:53 +0800

    add

--- project3.py
+++ project3.py
@@ -1,171 +1,101 @@
-import numpy as np
-import struct
-import matplotlib.pyplot as plt
-
-# 训练集文件
-train_images_idx3_ubyte_file = '/Users/yeeland/MLdata/MNIST_data/train-images-idx3-ubyte'
-# 训练集标签文件
-train_labels_idx1_ubyte_file = '/Users/yeeland/MLdata/MNIST_data/train-labels-idx1-ubyte'
-
-# 测试集文件
-test_images_idx3_ubyte_file = '/Users/yeeland/MLdata/MNIST_data/t10k-images-idx3-ubyte'
-# 测试集标签文件
-test_labels_idx1_ubyte_file = '/Users/yeeland/MLdata/MNIST_data/t10k-labels-idx1-ubyte'
+import torch
+from torch import nn, optim
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 
-def decode_idx3_ubyte(idx3_ubyte_file):
-    """
-    解析idx3文件的通用函数
-    :param idx3_ubyte_file: idx3文件路径
-    :return: 数据集
-    """
-    # 读取二进制数据
-    bin_data = open(idx3_ubyte_file, 'rb').read()
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 结构中只有输入和输出层
+        self.fc1 = nn.Linear(784, 10)
+        # 给一个激活函数,dim=1是第一个维度，即输出第一个维度的概率
+        self.softmax = nn.Softmax(dim=1)
 
-    # 解析文件头信息，依次为魔数、图片数量、每张图片高、每张图片宽
-    offset = 0
-    fmt_header = '>iiii' #因为数据结构中前4行的数据类型都是32位整型，所以采用i格式，但我们需要读取前4行数据，所以需要4个i。我们后面会看到标签集中，只使用2个ii。
-    magic_number, num_images, num_rows, num_cols = struct.unpack_from(fmt_header, bin_data, offset)
-    print('魔数:%d, 图片数量: %d张, 图片大小: %d*%d' % (magic_number, num_images, num_rows, num_cols))
-
-    # 解析数据集
-    image_size = num_rows * num_cols
-    offset += struct.calcsize(fmt_header)  #获得数据在缓存中的指针位置，从前面介绍的数据结构可以看出，读取了前4行之后，指针位置（即偏移位置offset）指向0016。
-    print(offset)
-    fmt_image = '>' + str(image_size) + 'B'  #图像数据像素值的类型为unsigned char型，对应的format格式为B。这里还有加上图像大小784，是为了读取784个B格式数据，如果没有则只会读取一个值（即一副图像中的一个像素值）
-    print(fmt_image,offset,struct.calcsize(fmt_image))
-    images = np.empty((num_images, num_rows, num_cols))
-    #plt.figure()
-    for i in range(num_images):
-        # if (i + 1) % 10000 == 0:
-        #     print('已解析 %d' % (i + 1) + '张')
-        #     print(offset)
-        images[i] = np.array(struct.unpack_from(fmt_image, bin_data, offset)).reshape((num_rows, num_cols))
-        #print(images[i])
-        offset += struct.calcsize(fmt_image)
-#        plt.imshow(images[i],'gray')
-#        plt.pause(0.00001)
-#        plt.show()
-    #plt.show()
-
-    return images
-
-
-def decode_idx1_ubyte(idx1_ubyte_file):
-    """
-    解析idx1文件的通用函数
-    :param idx1_ubyte_file: idx1文件路径
-    :return: 数据集
-    """
-    # 读取二进制数据
-    bin_data = open(idx1_ubyte_file, 'rb').read()
-
-    # 解析文件头信息，依次为魔数和标签数
-    offset = 0
-    fmt_header = '>ii'
-    magic_number, num_images = struct.unpack_from(fmt_header, bin_data, offset)
-    print('魔数:%d, 图片数量: %d张' % (magic_number, num_images))
-
-    # 解析数据集
-    offset += struct.calcsize(fmt_header)
-    fmt_image = '>B'
-    labels = np.empty(num_images)
-    for i in range(num_images):
-        if (i + 1) % 10000 == 0:
-            print ('已解析 %d' % (i + 1) + '张')
-        labels[i] = struct.unpack_from(fmt_image, bin_data, offset)[0]
-        offset += struct.calcsize(fmt_image)
-    return labels
-
-
-def load_train_images(idx_ubyte_file=train_images_idx3_ubyte_file):
-    """
-    TRAINING SET IMAGE FILE (train-images-idx3-ubyte):
-    [offset] [type]          [value]          [description]
-    0000     32 bit integer  0x00000803(2051) magic number
-    0004     32 bit integer  60000            number of images
-    0008     32 bit integer  28               number of rows
-    0012     32 bit integer  28               number of columns
-    0016     unsigned byte   ??               pixel
-    0017     unsigned byte   ??               pixel
-    ........
-    xxxx     unsigned byte   ??               pixel
-    Pixels are organized row-wise. Pixel values are 0 to 255. 0 means background (white), 255 means foreground (black).
-
-    :param idx_ubyte_file: idx文件路径
-    :return: n*row*col维np.array对象，n为图片数量
-    """
-    return decode_idx3_ubyte(idx_ubyte_file)
-
-
-def load_train_labels(idx_ubyte_file=train_labels_idx1_ubyte_file):
-    """
-    TRAINING SET LABEL FILE (train-labels-idx1-ubyte):
-    [offset] [type]          [value]          [description]
-    0000     32 bit integer  0x00000801(2049) magic number (MSB first)
-    0004     32 bit integer  60000            number of items
-    0008     unsigned byte   ??               label
-    0009     unsigned byte   ??               label
-    ........
-    xxxx     unsigned byte   ??               label
-    The labels values are 0 to 9.
-
-    :param idx_ubyte_file: idx文件路径
-    :return: n*1维np.array对象，n为图片数量
-    """
-    return decode_idx1_ubyte(idx_ubyte_file)
-
-
-def load_test_images(idx_ubyte_file=test_images_idx3_ubyte_file):
-    """
-    TEST SET IMAGE FILE (t10k-images-idx3-ubyte):
-    [offset] [type]          [value]          [description]
-    0000     32 bit integer  0x00000803(2051) magic number
-    0004     32 bit integer  10000            number of images
-    0008     32 bit integer  28               number of rows
-    0012     32 bit integer  28               number of columns
-    0016     unsigned byte   ??               pixel
-    0017     unsigned byte   ??               pixel
-    ........
-    xxxx     unsigned byte   ??               pixel
-    Pixels are organized row-wise. Pixel values are 0 to 255. 0 means background (white), 255 means foreground (black).
-
-    :param idx_ubyte_file: idx文件路径
-    :return: n*row*col维np.array对象，n为图片数量
-    """
-    return decode_idx3_ubyte(idx_ubyte_file)
-
-
-def load_test_labels(idx_ubyte_file=test_labels_idx1_ubyte_file):
-    """
-    TEST SET LABEL FILE (t10k-labels-idx1-ubyte):
-    [offset] [type]          [value]          [description]
-    0000     32 bit integer  0x00000801(2049) magic number (MSB first)
-    0004     32 bit integer  10000            number of items
-    0008     unsigned byte   ??               label
-    0009     unsigned byte   ??               label
-    ........
-    xxxx     unsigned byte   ??               label
-    The labels values are 0 to 9.
-
-    :param idx_ubyte_file: idx文件路径
-    :return: n*1维np.array对象，n为图片数量
-    """
-    return decode_idx1_ubyte(idx_ubyte_file)
-
+    def forward(self, x):
+        # 全连接层把(64, 1, 28, 28)转换为二维(64, 784),view相当于reshape,784=1*28*28
+        x = x.view(x.size()[0], -1)
+        x = self.fc1(x)
+        x = self.softmax(x)
+        return x
 
 
 if __name__ == '__main__':
-    train_images = load_train_images()
+    # 训练集
+    train_data = datasets.MNIST(root='./',
+                                train=True,
+                                transform=transforms.ToTensor(),
+                                download=True)
+    # 测试集
+    test_data = datasets.MNIST(root='./',
+                               train=False,
+                               transform=transforms.ToTensor(),
+                               download=True)
 
-    train_labels = load_train_labels()
-    # test_images = load_test_images()
-    # test_labels = load_test_labels()
+    # 批次大小，即一次加载多少数据
+    batch_size = 64
+    # 装载训练集，shuffle=True将数据打乱
+    train_load = DataLoader(dataset=train_data,
+                            batch_size=batch_size,
+                            shuffle=True)
+    # 装载测试集，将数据打乱
+    test_load = DataLoader(dataset=test_data,
+                           batch_size=batch_size,
+                           shuffle=True)
 
-    # 查看前十个数据及其标签以读取是否正确
+    # for i, data in enumerate(train_load):
+    #     inputs, labels = data
+    #     print(inputs.shape)
+    #     print(labels.shape)
+    #     print(labels)
+    #     break
+
+    # 定义模型,损失函数，优化器
+    model = Net()
+    mse_loss = nn.MSELoss()
+    opt = optim.SGD(model.parameters(), lr=0.5)
+
+
+    def train():
+        for i, data in enumerate(train_load):
+            # 每一次迭代都返回一组输入数据和标签
+            input_data, labels = data
+            # 获得模型的结果
+            out = model(input_data)
+            # (64)——>(64, 1)
+            labels = labels.reshape(-1, 1)
+            # 转换为独热编码
+            one_hot = torch.zeros(input_data.shape[0], 10).scatter(1, labels, 1)
+            # 计算loss,out, one_hot的shape要一致
+            loss = mse_loss(out, one_hot)
+            # 梯度清零
+            opt.zero_grad()
+            # 计算梯度
+            loss.backward()
+            # 修改权值
+            opt.step()
+
+
+    # 定义一个测试数据的函数
+    def test():
+        correct = 0
+        for i, data in enumerate(test_load):
+            # 每一次迭代都返回一组输入数据和标签
+            input_data, labels = data
+            # 获得模型的结果
+            out = model(input_data)
+            # 获得第一个维度的最大值，以及最大值所在的位置
+            max_value, pred_index = torch.max(out, 1)
+            # 用这64个预测数据与标签做一个对比，统计预测正确的数量
+            correct += (pred_index == labels).sum()
+
+        print('准确率：{0}'.format(correct.item() / len(test_data)))
+
+
+    # 训练和测试10个周期
     for i in range(10):
-        print(train_labels[i])
-        plt.imshow(train_images[i], cmap='gray')
-        plt.pause(0.000001)
-        plt.show()
-    print('done')
+        print(i, ':', end='')
+        train()
+        test()
+
